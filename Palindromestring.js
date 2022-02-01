7e3b2cd55810b79c5a2b700a055e9dbb0967f1f4
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-01 16:22:28 +0500

    writing code

--- Palindromestring.js
+++ Palindromestring.js
@@ -1,3 +1,18 @@
 //string code
-console.log("hello world");
-console.log("string palindrome");
+let ch = 12321,
+    rem, temp, reversenumber = 0;
+
+temp = number
+while (number > 0) {
+    rem = number % 10;
+    number = parseInt(number / 10);
+    console.log(number);
+    reversenumber = reversenumber * 10 + rem;
+}
+
+console.log(reversenumber);
+if (reversenumber == temp) {
+    console.log("number is palindrome");
+} else {
+    console.log("number is not palindrome");
+}
