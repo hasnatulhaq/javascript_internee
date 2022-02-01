428992319d93ea79a7eda51199ccab27873ba854
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-01 14:53:54 +0500

    palindrome code

---
+++ Palindrome.js
@@ -0,0 +1,17 @@
+let ch = 12321,
+    rem, temp, reversenumber = 0;
+
+temp = number
+while (number > 0) {
+    rem = number % 10;
+    number = parseInt(number / 10);
+    console.log(number);
+    reversenumber = reversenumber * 10 + rem;
+}
+
+console.log(reversenumber);
+if (reversenumber == temp) {
+    console.log("number is palindrome");
+} else {
+    console.log("number is not palindrome");
+}
