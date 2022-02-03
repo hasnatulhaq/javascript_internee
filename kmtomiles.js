c326d69b917f88efa52d36952fc2f265b1012e7a
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-03 18:03:04 +0500

    km to miles

--- kmtomiles.js
+++ kmtomiles.js
@@ -1,1 +1,13 @@
 // converting km to miles
+
+const km = 10,
+    factor = 0.621371;
+
+function kmtomiles(km) {
+    result = km * factor;
+    return result;
+}
+
+
+value = kmtomiles(km);
+console.log(value);
