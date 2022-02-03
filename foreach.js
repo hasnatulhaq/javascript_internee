3175f8de4850264a9addc3aae2bbce395d87fdfc
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-03 11:29:25 +0500

    foreach method

---
+++ foreach.js
@@ -0,0 +1,13 @@
+const arr = [
+    { color: 'red', code: 30 },
+    { color: 'green', code: 30 },
+    { color: 'white', code: 30 },
+    { color: 'yellow', code: 30 },
+    { color: 'pink', code: 30 },
+    { color: '', code: 30 },
+]
+
+
+arr.forEach((arr) => {
+    console.log(arr.color);
+})
