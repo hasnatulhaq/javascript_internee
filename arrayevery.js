ba2012980b14e7f6c35634b72bda0371d93646c1
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-03 15:08:35 +0500

    arrar every method

---
+++ arrayevery.js
@@ -0,0 +1,15 @@
+const items = [
+    { name: 'bike', price: 30 },
+    { name: 'car', price: 3000 },
+    { name: 'book', price: 100 },
+    { name: 'house', price: 300 },
+    { name: 'laptop', price: 500 },
+    { name: 'mobile', price: 200 },
+]
+
+
+const usingevery = items.every((items) => {
+    return items.price <= 100
+})
+
+console.log(usingevery);
