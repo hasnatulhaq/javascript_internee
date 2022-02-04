70c0803ad5c0ec9f58bdfbc5f43b354a68ac01ee
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-04 16:56:45 +0500

    array task

--- arraytask.js
+++ arraytask.js
@@ -1,1 +1,19 @@
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+//checking odd number 
+const oddnumber = number.filter((checkodd) => {
+
+    return checkodd % 2 !== 0;
+})
+
+console.log("odd number");
+//checking even number
+console.log(oddnumber);
+const evennumber = number.filter((checkeven) => {
+    checkeven = checkeven % 2 == 0;
+    return checkeven;
+})
+
+console.log(evennumber);
