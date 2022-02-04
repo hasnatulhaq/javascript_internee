80c4beec01963244b538386f24952e1c5d9fef1e
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-04 18:10:00 +0500

    array task

--- arraytask.js
+++ arraytask.js
@@ -1,19 +1,38 @@
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-
 //checking odd number 
 const oddnumber = number.filter((checkodd) => {
-
+    console.log(checkodd % 2 !== 0, "line no 7");
     return checkodd % 2 !== 0;
 })
-
 console.log("odd number");
-//checking even number
 console.log(oddnumber);
+
+//cheking even number
 const evennumber = number.filter((checkeven) => {
     checkeven = checkeven % 2 == 0;
     return checkeven;
 })
-
+console.log("even number");
 console.log(evennumber);
+
+//number divisble by 5
+const divbyfive = number.filter((checknumber) => {
+    checknumber = checknumber % 5 == 0;
+    return checknumber;
+})
+console.log("divisible by 5");
+console.log(divbyfive);
+console.log("////////////////////")
+    //An array of numbers divisible by 3 and then squared those numbers.
+const takingsquare = number.filter((checknumber) => {
+    console.log(checknumber);
+    number = checknumber % 3 == 0;
+    if (number === true) {
+        checknumber = checknumber * 2
+        return checknumber;
+    }
+
+})
+
+console.log(checknumber);
