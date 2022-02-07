ef2e0cc7d5b0a82d8ddbba3e54eeac8271d190a5
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-02-07 16:51:51 +0500

    arraymap

--- arraymap.js
+++ arraymap.js
@@ -1,7 +1,6 @@
 ///map method in array 
-
+/*
 const numberarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 
 const newarr = numberarr.map((val, index) => {
 
@@ -10,4 +9,26 @@ const newarr = numberarr.map((val, index) => {
 
 
 console.log(newarr);
-console.log(numberarr);
+console.log(numberarr);*/
+
+
+const customer = [
+    { id: 1, firstname: "Ahmed", city: "rawalpindi" },
+    { id: 2, firstname: "jamil", city: "Multan" },
+    { id: 3, firstname: "Naveed", city: "Islamabad" },
+    { id: 4, firstname: "Akhter", city: "karachi" },
+    { id: 5, firstname: "talha", city: "Lahore" },
+    { id: 6, firstname: "umair", city: "Quetta" },
+    { id: 7, firstname: "saeed", city: "peshawar" },
+    { id: 8, firstname: "jawad", city: "swat" },
+    { id: 9, firstname: "moiz", city: "Faisalabad" },
+    { id: 10, firstname: "waleed", city: "Sargodha" },
+    { id: 11, firstname: "hasnat", city: "Attock" },
+    { id: 12, firstname: "Hammad", city: "Jhang" },
+]
+
+
+const newarr = customer.map((val, index) => {
+
+    console.log(val);
+})
--- arrayreduce.js
+++ arrayreduce.js
@@ -1,5 +1,7 @@
 const numbers = [1, 2, 3, 4];
 const sum = numbers.reduce(function(result, item) {
+    console.log(item, "=", result)
+
     return result + item;
 }, 0);
 console.log(sum); // 10
